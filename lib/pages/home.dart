e1998979f396aebf626ca5bc985dbd2aae279f96
shaheenAdDU <snzsaladwani@addu.edu.ph>
2024-11-24 20:27:11 +0800

    Added footer and updated homepage look

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,18 +1,40 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 // Services, Models and Calls -- PLZ DONT TOUCH BRU
 import '/api/db_details.dart';
 import 'package:safe_stay/router/router.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+
 class Home extends StatelessWidget {
   const Home({super.key});
 
+  
+
   @override
   Widget build(BuildContext context) {
+
+    final List<String> districts = [
+      "Agdao",
+      "Baguio",
+      "Buhangin",
+      "Bunawan",
+      "Calinan",
+      "Marilog",
+      "Paquibato",
+      "Poblacion",
+      "Talomo",
+      "Toril",
+      "Tugbok",
+      "Toril" // This was missing!
+    ];
+    int sampleLoc = Random().nextInt(11);
+    
     return Scaffold(
       body: SingleChildScrollView(
         // Make the entire body scrollable
@@ -55,7 +77,7 @@ class Home extends StatelessWidget {
                       child: Text(
                         "LOGIN",
                         style: TextStyle(
-                          color: Color.fromARGB(255, 149, 149, 149),
+                          color: const Color.fromARGB(255, 149, 149, 149),
                           fontSize: 16,
                           fontFamily: GoogleFonts.raleway().fontFamily,
                           fontWeight: FontWeight.w600
@@ -126,54 +148,135 @@ class Home extends StatelessWidget {
                   ),
                   const SizedBox(height: 30),
                   // Inserted Card widget here
-                  Row(
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      const SizedBox(width: 30),
-                      Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius:
-                              BorderRadius.circular(16), // Rounded corners
-                        ),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Container(
-                              padding: const EdgeInsets.all(16),
-                              decoration: const BoxDecoration(
-                                color: Colors.green, // Green header
-                                borderRadius: BorderRadius.vertical(
-                                  top: Radius.circular(
-                                      16), // Rounded top corners
+                    Row(
+                      children: [
+                        const SizedBox(width: 30),
+                        Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.circular(16), // Rounded corners
+                          ),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.all(16),
+                                decoration: const BoxDecoration(
+                                  color:Color.fromARGB(255, 34, 124, 29), // Green header
+                                  borderRadius: BorderRadius.all(Radius.circular(16)),
                                 ),
-                              ),
-                              child: const Text(
-                                'ADD YOUR PROPERTY IN <FROM LOCATION> ',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    const SizedBox(width: 10),
+                                    Text(
+                                      'ADD YOUR PROPERTY IN',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.normal,
+                                        fontFamily: GoogleFonts.raleway().fontFamily,
+                                      ),
+                                    ),
+                                    const SizedBox(width: 10),
+                                    Text(
+                                      districts[sampleLoc],
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                        fontFamily: GoogleFonts.raleway().fontFamily,
+                                      ),
+                                    ),
+                                    const SizedBox(width: 200),
+                                    IconButton(
+                                      onPressed: () {}, 
+                                      icon: const FaIcon(
+                                        FontAwesomeIcons.house,
+                                        color: Colors.white)
+                                      ),
+                                    ],
+                                  )
                                 ),
-                              ),
+                              ],
                             ),
-                            Container(
-                              color: Colors.white, // White body
-                              padding: const EdgeInsets.all(16),
-                              child: const Column(
-                                children: [
-                                  SizedBox(height: 30),
-                                  Text('Sample Text'),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
                 ],
               ),
+              const SizedBox(height: 100),
+              _buildBelowCardContainer(),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBelowCardContainer() {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+      color: Colors.white,
+      child: Container(
+        padding: const EdgeInsets.all(25),
+        decoration: const BoxDecoration(
+          color:  Color.fromARGB(255, 34, 124, 29),
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+        ),
+        child: Column(
+          children: [
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween, 
+              children: [
+                Text(
+                  'This website is made by\nTADAYUKI HARUYAMA\nSHAHEEN AL ADWANI',
+                  style: TextStyle(color: Colors.white), 
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.facebook, color: Colors.white), 
+                    SizedBox(width: 10), // Spacing between icons
+                    Icon(Icons.g_translate, color: Colors.white), 
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 115),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    SizedBox(
+                      width: 50,
+                      height: 50,
+                      child: Image.network('https://qpetohtluhvwrrpletja.supabase.co/storage/v1/object/public/assets/logo.png'),
+                    ),
+                    const SizedBox(width: 5),
+                    const Text(
+                      'Safe Stay',
+                      style: TextStyle(
+                      fontFamily: 'Etna',
+                      fontSize: 23,
+                      color: Color.fromARGB(255, 255, 255, 255),
+                    ),
+                    ),
+                  ],
+                ),
+                const Text(
+                  'ALL RIGHTS RESERVED. © 2024',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
